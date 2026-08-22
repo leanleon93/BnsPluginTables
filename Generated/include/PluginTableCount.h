21d4bebeb8137f1cce371d36a79398ac60767de9
Leon W. <31262614+leanleon93@users.noreply.github.com>
2026-08-23 00:09:53 +0200

    Create PluginTableCount.h

---
+++ Generated/include/PluginTableCount.h
@@ -0,0 +1,9 @@
+/*
+ Generated Code! Do not manually edit this code. Modify the generator instead.
+*/
+#pragma once
+#ifdef BNSKR
+	#define BNS_PLUGIN_TABLE_COUNT_OVERRIDE_VALUE 516
+#else
+	#define BNS_PLUGIN_TABLE_COUNT_OVERRIDE_VALUE 516
+#endif
